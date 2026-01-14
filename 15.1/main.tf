d084d7853fc2b1c28325e2da0975bb5fc09d41b5
EremeevAN <lexa16042010@mail.ru>
2026-01-15 01:25:53 +0300

    Add files via upload

---
+++ 15.1/main.tf
@@ -0,0 +1,141 @@
+locals {
+  network_name     = "develop"
+  subnet_name1     = "public"
+  subnet_name2     = "private"
+  sg_nat_name      = "nat-instance-sg"
+  vm_test_name     = "test-vm"
+  vm_nat_name      = "nat"
+  route_table_name = "nat-route"
+  ssh_key_path = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcgt/AOnVvE8jIXBvqvW2o3J91v26dcRRxI/O03mEgf user@WIN-SQL1"
+}
+
+resource "yandex_vpc_network" "develop" {
+  name = local.network_name
+}
+
+resource "yandex_vpc_subnet" "public" {
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.develop.id
+  name = "public"
+}
+
+resource "yandex_vpc_subnet" "private" {
+  v4_cidr_blocks = ["192.168.20.0/24"]
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.develop.id
+  route_table_id = yandex_vpc_route_table.nat-route.id
+  name = "private"
+}
+resource "yandex_vpc_security_group" "nat-instance-sg" {
+  name       = local.sg_nat_name
+  network_id = yandex_vpc_network.develop.id
+
+  egress {
+    protocol       = "ANY"
+    description    = "Всё"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "ssh"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "http"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "https"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+}
+
+resource "yandex_compute_image" "nat-instance-ubuntu" {
+  source_family = "nat-instance-ubuntu"
+}
+
+resource "yandex_compute_disk" "boot-disk-vm" {
+  name     = "boot-disk-vm"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = "20"
+  image_id = yandex_compute_image.nat-instance-ubuntu.id
+}
+
+resource "yandex_compute_disk" "boot-disk-nat" {
+  name     = "boot-disk-nat"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = "20"
+  image_id = yandex_compute_image.nat-instance-ubuntu.id
+}
+
+resource "yandex_compute_instance" "test-vm" {
+  name        = local.vm_test_name
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+
+  resources {
+    core_fraction = 20
+    cores         = 2
+    memory        = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-vm.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.private.id
+    security_group_ids = [yandex_vpc_security_group.nat-instance-sg.id]
+  }
+
+  metadata = {
+    user-data = "#cloud-config\nusers:\n  - name: ${var.vm_user_nat}\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=(ALL) NOPASSWD:ALL'\n    ssh-authorized-keys:\n      - ${local.ssh_key_path}"
+  }
+}
+
+resource "yandex_compute_instance" "nat-instance" {
+  name        = local.vm_nat_name
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+
+  resources {
+    core_fraction = 20
+    cores         = 2
+    memory        = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-nat.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    ip_address = "192.168.10.254"
+    security_group_ids = [yandex_vpc_security_group.nat-instance-sg.id]
+    nat                = true
+  }
+
+  metadata = {
+    user-data = "#cloud-config\nusers:\n  - name: ${var.vm_user_nat}\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=(ALL) NOPASSWD:ALL'\n    ssh-authorized-keys:\n      - ${local.ssh_key_path}"
+  }
+}
+
+resource "yandex_vpc_route_table" "nat-route" {
+  name       = "nat-route"
+  network_id = yandex_vpc_network.develop.id
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    next_hop_address   = yandex_compute_instance.nat-instance.network_interface.0.ip_address
+  }
+}
